295a48106a1679f4b8778f56b3e2d5d83a2c1d72
DESKTOP-C5Q08DJ\XxxAngelockxxX <gigievgi254@gmail.com>
2025-03-19 14:14:59 +0200

    LB 2

---
+++ LB 2/LB 2/Moderator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LB_2
+{
+    internal class Moderator:User
+    {
+        public Moderator(string email, string username, string password):base(email, username, password)
+        {
+            
+        }
+        public void ModerateContent()
+        {
+            Console.WriteLine("Контент модеровано.");
+        }
+        public override void DisplayInfo()
+        {
+            Console.WriteLine("Роль: Модератор");
+        }
+    }
+}
